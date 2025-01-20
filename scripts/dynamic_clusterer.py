a719ea0f647d2a4a35f64e2925d92b901f18a61b
mirkomanset <mansetmirko26@gmail.com>
2025-01-20 12:24:46 +0100

    Initial commit: add DynamicClusterer v8

---
+++ scripts/dynamic_clusterer.py
@@ -0,0 +1,316 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import copy
+from random import randint
+import imageio
+import os
+
+from river import stream, drift
+from scripts.utils import extract_integer, is_in_any_sublist, find_missing_positive, get_snapshot_image, keep_first_occurrences, internal_transition
+from scripts.tracker import MEC
+
+
+# Snapshot class to keep the information about the current situation of micro/macro clusters and model
+
+class Snapshot:
+
+  def __init__(self, microclusters, macroclusters, model, k, timestamp):
+
+    self.microclusters = microclusters
+    self.macroclusters = macroclusters
+    self.timestamp = timestamp
+    self.model = model
+    self.k = k
+
+def compute_avg_distance(x, microclusters):
+  temp_list = []
+  for mc in microclusters:
+    point = list(x.values())
+    temp_list.append(np.linalg.norm(np.array(point) - np.array(mc)))
+  return min(temp_list)
+
+
+# Main Class that wrapped the model, data and clustering
+
+class DynamicClusterer:
+
+  # Initialization: it receives the reference data and the model and initializes the instance
+  def __init__(self, data, model, colors, dist_t_factor=0.9, radius_t_factor=0.5, x_limits=(-5,20), y_limits=(-5,20), threshold=10):
+
+    self.model = model
+    self.colors = colors
+    self.data = data
+    self.timestamp = 0
+
+    self.x_limits = x_limits
+    self.y_limits = y_limits
+
+    self.drift_detector = drift.PageHinkley(threshold=threshold)
+
+    self.id = randint(10000, 99999)
+    print(f'New model created - id: {self.id}')
+
+    self.directory = f'/content/{self.id}'
+    os.makedirs(self.directory, exist_ok=True)
+
+    # Fit model into reference data
+    for x, _ in stream.iter_array(self.data):
+      self.model.learn_one(x)
+
+    # Apply macroclustering on reference
+    self.model.apply_macroclustering()
+
+    # Number of macroclusters
+    self.k = self.model.best_k
+
+    # Save a list of macroclusters
+    self.macroclusters = []
+    centers = [d['center'] for d in self.model.macroclusters]
+    radii = [d['radius'] for d in self.model.macroclusters]
+    for i in range(len(self.model.macroclusters)):
+      macrocluster = {'id':i, 'center':centers[i], 'radius':radii[i]}
+      self.macroclusters.append(macrocluster)
+
+    # Set of microclusters
+    self.microclusters = self.model.get_microclusters()
+
+    # Initialize PHT
+    for x, _ in stream.iter_array(self.data):
+      dist = compute_avg_distance(x, self.microclusters)
+      self.drift_detector.update(dist)
+
+    # Snapshot mechanism to keep trace of the relevant changes
+    self.snapshots = []
+    snapshot = Snapshot(copy.deepcopy(self.microclusters), copy.deepcopy(self.macroclusters), copy.deepcopy(self.model), copy.deepcopy(self.k), copy.deepcopy(self.timestamp))
+    self.snapshots.append(snapshot)
+
+    # Parameters for detecting relevant internal changes
+    self.dist_threshold_factor = dist_t_factor
+    self.radius_threshold_factor = radius_t_factor
+
+    # Data for prod
+    self.prod = []
+
+    # Saved plots
+    self.plots = []
+
+    # Print the reference clustering
+    self.print_macro_cluster()
+
+    # Plot reference clustering
+    self.plot_clustered_data(plot_img=True)
+
+  # Get centers and radii
+  def get_macroclusters_centers(self):
+    return [d['center'] for d in self.macroclusters]
+
+  def get_macroclusters_radii(self):
+    return [d['radius'] for d in self.macroclusters]
+
+
+  # Print macrocluster informations
+  def print_macro_cluster(self):
+    for element in self.macroclusters:
+      print(element)
+
+
+  # Update prod data
+  def receive_prod(self, data):
+    self.prod = data
+
+  # Fit prod data
+  def fit_prod_data(self, print_statistics=False, print_results=False, print_graph=False, plot_img=True, macroclustering_at_end=True):
+
+    # Fit the new data: online phase
+    for x, _ in stream.iter_array(self.prod):
+      self.timestamp += 1
+      self.model.learn_one(x)
+      dist = compute_avg_distance(x, self.microclusters)
+      self.drift_detector.update(dist)
+      if self.drift_detector.drift_detected:
+        print(f"<!> Change detected! Possible input drift at timestamp {self.timestamp} ----> Apply macroclustering <!>")
+        self.trigger_macroclustering(print_statistics=print_statistics, print_results=print_results, print_graph=print_graph, plot_img=plot_img)
+
+    # Apply macroclustering at the end of the batch
+    # Note that we do not save the the new macroclustering now
+    if macroclustering_at_end:
+      print('Batch Finished ----> Apply macroclustering')
+      self.trigger_macroclustering(print_statistics=print_statistics, print_results=print_results, print_graph=print_graph, plot_img=plot_img)
+
+
+
+  def trigger_macroclustering(self, print_statistics=False, print_results=False, print_graph=False, plot_img=True):
+
+    relevant_change_flag = False # Flag to find if a relevant change is detected
+
+    self.model.apply_macroclustering()
+
+    # Update microclusters and new number of macrocluster
+    self.microclusters = self.model.get_microclusters()
+    self.k = self.model.best_k
+
+    # Prod data is cleaned
+    self.prod = []
+
+    # Track transitions performed by MEC
+    # We compare the new clustering with the actual situation
+    G = MEC(self.macroclusters, self.model.macroclusters, print_statistics=print_statistics, print_results=print_results, print_graph=print_graph)
+
+    # Find mapping between current clustering and new clustering
+    mapping = {}
+    for edge in G.edges():
+        left_node, right_node = edge
+        mapping.setdefault(extract_integer(right_node), []).append(extract_integer(left_node))
+
+    # Remove disappearing clusters
+    values_list = list(mapping.values())
+    to_remove = []
+    for cur_cluster in self.macroclusters:
+      if is_in_any_sublist(cur_cluster['id'], values_list)==False:
+        relevant_change_flag = True
+        to_remove.append(cur_cluster)
+    if len(to_remove) > 0:
+      relevant_change_flag = True
+      for r in to_remove:
+        self.macroclusters.remove(r) # Update our macroclusters by removing the disappearing ones
+        print(f'(!) {r["center"]} disappeared')
+
+    # Manage surviving clusters
+    surviving_clusters = []
+    old_macroclusters = copy.deepcopy(self.macroclusters) # Save a copy of current macroclustering for comparisons while updating the real instance
+
+    for j in range(len(self.model.macroclusters)):
+      new_cluster = self.model.macroclusters[j] # new_cluster: each cluster find in the next clustering
+
+      # Surviving/splitting macroclusters update
+      if j in mapping and len(mapping[j]) == 1:
+        for i in range(len(old_macroclusters)):
+          old_cluster = old_macroclusters[i] # old_cluster: each cluster of the current clustering
+          if old_cluster['id'] == mapping[j][0]:
+            # Manage surviving
+            if old_cluster['id'] not in surviving_clusters:
+
+              print(f'{old_cluster["center"]} survived as {new_cluster["center"]}')
+
+              # In order to detect a relevant change we compare the clustering with the clustering of the last snapshot
+              # Referential clustering to detect shifting and dimensional changes
+              last_snapshot = self.snapshots[-1]
+              # Compare the internal transition with the last snapshot
+              for l in range(len(last_snapshot.macroclusters)):
+                last_snapshot_cluster = last_snapshot.macroclusters[l]
+                # When the corresponding cluster in the last snapshot clustering is found
+                if last_snapshot_cluster['id'] == mapping[j][0]:
+                  max_dist = self.dist_threshold_factor * max(last_snapshot_cluster['radius'], new_cluster['radius'])
+                  dist, r_ratio = internal_transition(last_snapshot_cluster, new_cluster)
+                  break
+
+              if dist > max_dist: # type: ignore
+                print(f'(!) But a relevant center movement detected: dist={dist}')
+                relevant_change_flag = True
+
+              if r_ratio < self.radius_threshold_factor or r_ratio > 1/self.radius_threshold_factor:
+                print(f'(!) But a relevant radius update detected: ratio={r_ratio}')
+                relevant_change_flag = True
+
+              # Update surviving macroclusters
+              surviving_clusters.append(old_cluster['id'])
+              self.macroclusters[i]['center'] = new_cluster["center"]
+              self.macroclusters[i]['radius'] = new_cluster["radius"]
+
+
+            # Manage splitting
+            # Splitting happens when a cluster survives in 2 or more clusters
+            # The first one is updated, the other are added to currant clustering following the lowest ID policy
+            else:
+              print(f'(!) {old_cluster["center"]} survived as {new_cluster["center"]} but a splitting is needed')
+              relevant_change_flag = True
+              current_ids_list = []
+              for i in range(len(self.macroclusters)):
+                current_ids_list.append(self.macroclusters[i]['id'])
+              new_id = find_missing_positive(current_ids_list)
+              self.macroclusters.append({'id':new_id, 'center':new_cluster['center'], 'radius':new_cluster['radius']})
+            break
+
+      # Manage merging clusters
+      # It happens whe 2 or more clusters survive in the same cluster
+      # We update every cluster and later the duplicates are removed
+      elif j in mapping and len(mapping[j]) > 1:
+        relevant_change_flag = True
+        merged_list = []
+        for m in self.macroclusters:
+          for k in range(len(mapping[j])):
+            if m['id'] == mapping[j][k]:
+              merged_list.append(m['center'])
+              m['center'] = new_cluster["center"]
+              m['radius'] = new_cluster["radius"]
+        print(f'(!) {merged_list} are merged in {new_cluster["center"]}')
+
+      # Manage appearing cluster
+      else:
+        relevant_change_flag = True
+        current_ids_list = []
+        for i in range(len(self.macroclusters)):
+          current_ids_list.append(self.macroclusters[i]['id'])
+        new_id = find_missing_positive(current_ids_list)
+        self.macroclusters.append({'id':new_id, 'center':new_cluster['center'], 'radius':new_cluster['radius']})
+        print(f'(!) {new_cluster["center"]} appeared')
+
+    # Remove duplicates to handle merging clusters
+    self.macroclusters = keep_first_occurrences(self.macroclusters)
+
+    # If relevant change is detected we append a new snapshot
+    
+    relevant_change_flag = True ##############àà Set it always relevann
+    if relevant_change_flag:
+      print()
+      print(f'(!) Relevant change in macroclustering detected new snaphost at timestamp: {self.timestamp} -----> Update referential result')
+      print()
+      print('-----------------------------------------------------------------------------------')
+      print()
+      snapshot = Snapshot(copy.deepcopy(self.microclusters), copy.deepcopy(self.macroclusters), copy.deepcopy(self.model), copy.deepcopy(self.k), copy.deepcopy(self.timestamp))
+      self.snapshots.append(snapshot)
+
+    # Call plotting whenever we fit the new prof
+    self.plot_clustered_data(plot_img=plot_img)
+
+
+  # Plot clustered microclusters and macroclusters
+  def plot_clustered_data(self, plot_img=True):
+    snapshot = Snapshot(copy.deepcopy(self.microclusters), copy.deepcopy(self.macroclusters), copy.deepcopy(self.model), copy.deepcopy(self.k), copy.deepcopy(self.timestamp))
+    fig = get_snapshot_image(snapshot, self.colors, x_limits=self.x_limits, y_limits=self.y_limits)
+    # Plot only if needed (False by default)
+    # Save image in the directory of this specific instance (ID) (True by default)
+    # Append also in the plots list
+    fig.savefig(f'{self.directory}/temp_image_{len(self.plots)}.png')
+    self.plots.append(f'{self.directory}/temp_image_{len(self.plots)}.png')
+    if plot_img:
+      plt.show()
+    plt.close('all')
+    del snapshot
+
+  # Get model
+  # Useful to call tracking externally
+  def get_model(self):
+    return self.model
+
+  # Clean plots if they are no more needed
+  def clean_plots(self):
+    for filename in self.plots:
+      os.remove(filename)
+    self.plots = []
+
+  # Draw gif
+  def draw_gif(self, title=''):
+    with imageio.get_writer(f'/content/{self.id}/{title}.gif', mode='I', duration=1000) as writer:
+      for filename in self.plots:
+          image = imageio.v2.imread(filename)
+          writer.append_data(image)
+
+  # Draw only the snapshots
+  def draw_snapshots(self, plot_img=True):
+    for snapshot in self.snapshots:
+      fig = get_snapshot_image(snapshot, self.colors, x_limits=self.x_limits, y_limits=self.y_limits)
+      fig.savefig(f'{self.directory}/snapshot_{snapshot.timestamp}.png')
+      if plot_img:
+        plt.show()
+        plt.close('all')
---
+++ scripts/tracker.py
@@ -0,0 +1,89 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import networkx as nx
+
+
+
+# MEC algorithm for tracking
+# Based on overlapping and bipartite graph
+
+def MEC(clusters_ref, clusters_prod, print_statistics=False, print_results=False, print_graph=False):
+  n_clusters_ref = len(clusters_ref)
+  n_clusters_prod = len(clusters_prod)
+
+  G = nx.Graph()
+  color_map = []
+  active_clusters_ref = []
+  active_clusters_prod = []
+
+  for i in range(n_clusters_ref):
+      c_name_ref = 'ref' + str(clusters_ref[i]['id'])
+      G.add_node(c_name_ref, bipartite=0)
+      active_clusters_ref.append(clusters_ref[i]['id'])
+      color_map.append('lightskyblue')
+
+  for i in range(n_clusters_prod):
+      c_name_prod = 'prod' + str(i)
+      G.add_node(c_name_prod, bipartite=1)
+      active_clusters_prod.append(i)
+      color_map.append('limegreen')
+
+  #print(f'Active clusters in reference: {list(set(active_clusters_ref))} - Active clusters in prod: {list(set(active_clusters_prod))}')
+  print()
+
+  centers_distances = {}
+  radius_difference = {}
+
+  for i in range(n_clusters_ref):
+    cref_radius = clusters_ref[i]['radius']
+    cref_center = clusters_ref[i]['center']
+
+    for j in range(n_clusters_prod):
+      cprod_radius = clusters_prod[j]['radius']
+      cprod_center = clusters_prod[j]['center']
+
+      dist = np.linalg.norm(np.array(cref_center) - np.array(cprod_center))
+
+      if print_statistics:
+        print(f'ref{clusters_ref[i]["id"]} - center: {cref_center} - radius: {cref_radius}')
+        print(f'prod{j} - center: {cprod_center} - radius: {cprod_radius}')
+        print(f'distance of centers: {dist} - sum of radius: {cref_radius + cprod_radius}',)
+        print()
+
+      if dist < (cref_radius + cprod_radius):
+        c_name_ref = 'ref' + str(clusters_ref[i]['id'])
+        c_name_prod = 'prod' + str(j)
+        G.add_edge(c_name_ref, c_name_prod)
+
+
+        centers_distances[f'{c_name_ref}{c_name_prod}'] = dist
+        radius_difference[f'{c_name_ref}{c_name_prod}'] = cref_radius - cprod_radius
+
+  if print_graph:
+    nx.draw(G, node_color=color_map, with_labels=True)
+    plt.show()
+
+  for i in range(len(active_clusters_ref)):
+    active_clusters_ref[i] = 'ref' + str(clusters_ref[i]['id'])
+
+  for i in range(len(active_clusters_prod)):
+    active_clusters_prod[i] = 'prod' + str(active_clusters_prod[i])
+
+  if print_results:
+    # Print the degree of each node in the top set
+    for node in G.nodes():
+      neighbors = list(G.neighbors(node))
+      if node in active_clusters_ref and len(neighbors) == 0:
+        print(f'{node} disappeared')
+      elif node in active_clusters_ref and len(neighbors) == 1:
+        print(f'{node} survived as {neighbors[0]} - Center moved of {centers_distances[f"{node}{neighbors[0]}"]} - Radius changed of {radius_difference[f"{node}{neighbors[0]}"]}')
+      elif node in active_clusters_ref and len(neighbors) > 1:
+        print(f'{node} is splitted in {neighbors}')
+      elif node in active_clusters_prod and len(neighbors) == 0:
+        print(f'{node} appeared')
+      #elif node in active_clusters_prod and len(neighbors) == 1:
+        #print(f'{node} is the new version of {neighbors[0]}')
+      elif node in active_clusters_prod and len(neighbors) > 1:
+        print(f'{node} is the result of the merging of in {neighbors}')
+
+  return G
